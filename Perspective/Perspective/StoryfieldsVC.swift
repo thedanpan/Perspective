770789413fd8305df055fc5849237ce2466a3d0f
Scott Enriquez & Daniella Shin <pair+sjenriquez.thedanpan@devbootcamp.com>
2015-02-15 15:39:09 -0500

    save story object successfully in database

--- Perspective/Perspective/StoryfieldsVC.swift
+++ Perspective/Perspective/StoryfieldsVC.swift
@@ -12,20 +12,50 @@ class StoryfieldsVC: UIViewController {
     
     @IBOutlet weak var theme: UITextField!
     @IBOutlet weak var clipDuration: UITextField!
-    @IBOutlet weak var numofClips: UITextField!
+    @IBOutlet weak var numOfClips: UITextField!
     @IBOutlet weak var searchFriends: UISearchBar!
 
+    @IBOutlet weak var txtIncompleteFieldsMessage: UILabel!
+    
+    
+    @IBAction func submitStoryfields(sender: AnyObject) {
+        var themeEntered = theme.text
+        var clipDurationEntered = clipDuration.text.toInt()
+        var numOfClipsEntered = numOfClips.text.toInt()
+        
+        func createStory(){
+            var story = PFObject(className: "Story")
+            story["theme"] = themeEntered
+            story["clipDuration"] = clipDurationEntered
+            story["numOfClips"] = numOfClipsEntered
+            story.saveInBackgroundWithBlock {
+                (success: Bool, error: NSError!) -> Void in
+                if (success) {
+                    // The object has been saved.
+                } else {
+                    // There was a problem, check error.description
+                }
+            }
+        }
+        
+        if themeEntered != "" && clipDurationEntered != nil && numOfClipsEntered != nil {
+            createStory()
+        } else {
+            self.txtIncompleteFieldsMessage.text = "All Fields Required"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -36,5 +66,4 @@ class StoryfieldsVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }

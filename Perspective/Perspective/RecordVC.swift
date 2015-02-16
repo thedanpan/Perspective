500122e9fa2d0ff1898e51b9582a6e8b1ab692f2
Darrin Reeves & Scott Enriquez <pair+dxr4841.sjenriquez@devbootcamp.com>
2015-02-16 15:02:47 -0500

    Create S3 files using UNIX timestamp, assign current user, comment out saving video to camera roll.

--- Perspective/Perspective/RecordVC.swift
+++ Perspective/Perspective/RecordVC.swift
@@ -13,59 +13,104 @@ import MobileCoreServices
 
 
 class RecordVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
-    
+
     let captureSession = AVCaptureSession()
+    let date = NSDate()
+
     var previewLayer : AVCaptureVideoPreviewLayer?
     var captureDevice : AVCaptureDevice?
-    
+    var currentUser = PFUser.currentUser()
+
+
     override func viewDidAppear(animated: Bool) {
-        
+
         captureSession.sessionPreset = AVCaptureSessionPreset640x480
-        
+
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            
+
             println("captureVideoPressed and camera available.")
-            
+
             var imagePicker = UIImagePickerController()
-            
+
             imagePicker.delegate = self
-            
+
             imagePicker.sourceType = .Camera;
-            
+
             imagePicker.mediaTypes = [kUTTypeMovie!]
-            
+
             imagePicker.allowsEditing = false
-            
+
             imagePicker.showsCameraControls = true
-            
+
             self.presentViewController(imagePicker, animated: true, completion: nil)
-            
+
         }
         else {
             println("Camera not available.")
         }
     }
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info:NSDictionary!) {
-        
-        
+
+        let timestamp = date.timeIntervalSince1970
         let tempImage = info[UIImagePickerControllerMediaURL] as NSURL!
         let pathString = tempImage.relativePath
+        let fileUrl = info[UIImagePickerControllerMediaURL] as NSURL!
+        var uploadRequest:AWSS3TransferManagerUploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest.bucket = "theperspectiveapp"
+        uploadRequest.key = "\(timestamp).mp4"
+        uploadRequest.contentType = "video/mp4"
+        uploadRequest.body = fileUrl
+        uploadRequest.uploadProgress = { (bytesSent:Int64, totalBytesSent:Int64,  totalBytesExpectedToSend:Int64) -> Void in
+            dispatch_sync(dispatch_get_main_queue(), {() -> Void in
+                println(totalBytesSent)
+            })
+        }
+
+        AWSS3TransferManager.defaultS3TransferManager().upload(uploadRequest).continueWithBlock { (task) -> AnyObject! in
+            if (task.error != nil) {
+                //failed
+                println("failed")
+                println(task.error.code)
+                println(task.error.localizedDescription)
+            } else {
+                //completed
+                println("completed")
+                self.createVideoParseObj(uploadRequest.key)
+            }
+            return nil
+        }
+
         self.dismissViewControllerAnimated(true, completion: {})
-        
-        UISaveVideoAtPathToSavedPhotosAlbum(pathString, self, nil, nil)
-        
+        // UISaveVideoAtPathToSavedPhotosAlbum(pathString, self, nil, nil)
     }
-    
+
+    func createVideoParseObj(key: String) {
+        var video = PFObject(className:"Video")
+        video["url"] = "https://s3.amazonaws.com/theperspectiveapp/\(key)"
+        video["user"] = "\(currentUser.username)"
+        video.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError!) -> Void in
+            if (success) {
+                // The object has been saved.
+                println("saved to Parse")
+            } else {
+                // There was a problem, check error.description
+
+            }
+        }
+    }
+
 }

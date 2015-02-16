878650b66ca1e3053204a134177ae35886dad399
Darrin Reeves & Scott Enriquez <pair+dxr4841.sjenriquez@devbootcamp.com>
2015-02-16 17:44:55 -0500

    Restore missing frameworks.

---
+++ Perspective/Perspective-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Parse/Parse.h>
+#import <Bolts/Bolts.h>
+#import <AWSiOSSDKv2/AWSCore.h>
+#import <AWSiOSSDKv2/S3.h>
+#import <AWSiOSSDKv2/DynamoDB.h>
+#import <AWSiOSSDKv2/SQS.h>
+#import <AWSiOSSDKv2/SNS.h>

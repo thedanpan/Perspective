a021fbd2de5923afcb87f48e457040e910e68e22
Scott Enriquez <sjenriquez@gmail.com>
2015-02-18 18:31:35 -0500

    Create notification after saving new Perspective.

--- Perspective/Perspective/PerspectiveFieldsVC.swift
+++ Perspective/Perspective/PerspectiveFieldsVC.swift
@@ -15,27 +15,42 @@ class PerspectiveFieldsVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchFriends: UISearchBar!
     @IBOutlet weak var submit: UIButton!
     var url: String!
+    var toUser: String!
 
     @IBOutlet weak var txtIncompleteFieldsMessage: UILabel!
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        theme.endEditing(true)
+        numOfClips.endEditing(true)
+    }
     
     @IBAction func submitPerspectiveFields(sender: AnyObject) {
         var themeEntered = theme.text
         var numOfClipsEntered = numOfClips.text.toInt()
         
         func createPerspective(){
-            var currentUser = PFUser.currentUser()
-            
             var perspective = PFObject(className: "Perspective")
             perspective["theme"] = themeEntered
             perspective["numOfClips"] = numOfClipsEntered
             perspective["collaborators"] = [PFUser.currentUser().username]
             perspective["videos"] = [url]
-//            perspective.addUniqueObjectsFromArray([url], forKey:"videos")
             
             perspective.saveInBackgroundWithBlock {
                 (success: Bool, error: NSError!) -> Void in
                 if (success) {
+                    var perspectiveId = perspective.objectId
+                    self.createNotification(perspectiveId)
                     println("Sucessfully saved Perspective")
                 } else {
                     println("Perspective did not save")
@@ -52,15 +67,20 @@ class PerspectiveFieldsVC: UIViewController, UITextFieldDelegate {
      
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        println("the url is: \(self.url)")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createNotification(perspectiveId: String) {
+        var notification = PFObject(className: "Notification")
+        notification["fromUser"] = PFUser.currentUser().username
+        notification["toUser"] = "darrin"
+        notification["notificationType"] = "invite"
+        notification["perspectiveId"] = perspectiveId
+        notification.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError!) -> Void in
+            if (success) {
+                println("Sucessfully saved Notification")
+            } else {
+                println("Notification did not save")
+            }
+        }
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -68,10 +88,7 @@ class PerspectiveFieldsVC: UIViewController, UITextFieldDelegate {
         return true;
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        theme.endEditing(true)
-        numOfClips.endEditing(true)
-    }
+
 
     /*
     // MARK: - Navigation

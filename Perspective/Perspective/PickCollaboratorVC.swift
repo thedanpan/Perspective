66d8d34a989d1de08d8271540db3b9bf9b2ea22d
Darrin Reeves & Scott Enriquez <pair+dxr4841.sjenriquez@devbootcamp.com>
2015-02-18 12:54:05 -0500

    Add whosnextvc that updates perspective with new collaborator info and check for completion.

---
+++ Perspective/Perspective/PickCollaboratorVC.swift
@@ -0,0 +1,35 @@
+//
+//  PickCollaboratorVC.swift
+//  Perspective
+//
+//  Created by Apprentice on 2/18/15.
+//  Copyright (c) 2015 Dev Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class PickCollaboratorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Perspective/Perspective/WhosNextVC.swift
+++ Perspective/Perspective/WhosNextVC.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class WhosNextVC: UIViewController {
     
+//    @IBOutlet weak var instructionsText: UILabel!
+    
     var url: String!
+    var perspectiveId: String!
+    var receiver : String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        instructionsText.text = "Pick the next collaborator"
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +27,50 @@ class WhosNextVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func gotoPickCollaborator(sender: UIButton) {
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("PickCollaboratorVC") as PickCollaboratorVC
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    */
-
+    
+    func setPerspectiveComplete(perspective: PFObject) {
+        perspective["complete"] = true
+        perspective.saveInBackground()
+    }
+    
+    @IBAction func queryPerspective (sender: UIButton) {
+        var query = PFQuery(className: "Perspective")
+        query.getObjectInBackgroundWithId(self.perspectiveId) {
+            (perspective: PFObject!, error: NSError!) -> Void in
+            if error != nil {
+                NSLog("%@", error)
+                println("Error, Perspective not retrieved.")
+            } else {
+                perspective.addObject(PFUser.currentUser().username, forKey: "collaborators")
+                perspective.addObject(self.url, forKey: "videos")
+                perspective.saveInBackgroundWithBlock {
+                    (success: Bool, error: NSError!) -> Void in
+                    if (success) {
+                        let videosArray:NSArray = perspective["videos"] as NSArray
+                        let videosArrayCount: Int = videosArray.count
+                        println(videosArrayCount)
+                        let perspectiveNumOfClips: Int = perspective["numOfClips"] as Int
+                        println(perspectiveNumOfClips)
+                        if perspectiveNumOfClips == videosArrayCount {
+//                          CREATE COMPLETION NOTIFICATION
+                            println("Inside completion check - if")
+                            self.setPerspectiveComplete(perspective)
+                          } else {
+                            println("Inside completion check - else")
+//                          CREATE NOTIFICATION FOR NEXT COLLABORATOR
+                          }
+                    } else {
+                        NSLog("%@", error)
+                        println("Error, Perspective not updated.")
+                    }
+                }
+            }
+                self.navigationController?.popToRootViewControllerAnimated(true)
+                println("Perspective updating in background, pop to root VC.")
+        }
+    }
 }

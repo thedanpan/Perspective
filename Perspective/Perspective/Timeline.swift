1d4dad1daacba3c9eb5ee4ca07206c85334b05ef
Darrin Reeves <dxr4841@gmail.com>
2015-02-18 21:30:43 -0500

    Perspective/Perspective/Base.lproj/Main.storyboard

---
+++ Perspective/Perspective/Timeline.swift
@@ -0,0 +1,24 @@
+//
+//  Timeline.swift
+//  Perspective
+//
+//  Created by Apprentice on 2/18/15.
+//  Copyright (c) 2015 Dev Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class Timeline: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
